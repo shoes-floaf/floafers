c7f41887082af7f6ed077a52224497d0dc1767d6
shoes-floaf <toysway1@gmail.com>
2024-08-22 10:01:19 +0300

    1

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,8 +9,8 @@ export default function About(){
     <p>נותרו זוגות  בודדים , הזמינו מהר לפני שייגמר ...</p>
     <p>עבור כל דגם מופיע מידת נעל שנותרה</p>
     <p>בחרי לך </p>
-    <p>ושלחי     <a href="mailto:toysway1@gmail.com?subject=הזמנת%20נעל%20פלופרס" target="_blank">    למייל זה    </a>
-      את דגם הנעל והמידה הרצויה  </p>
+    <p>ושלחי     <a href="mailto:toysway1@gmail.com?subject=הזמנת%20נעל%20פלופרס" target="_blank">   למייל זה    </a>
+   <span> (toysway1@gmail.com)</span>  את דגם הנעל והמידה הרצויה    </p>
       <p>ואת שם המשפחה והעיר </p>
       <p>* משלוחים בתיאום עד יום שני כ"ב אב</p>
      </>
