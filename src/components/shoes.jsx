c9cd63dc5877bd407e91cfcc689e70ca2c55a105
chanaGreenfeld <c64966@gmail.com>
2025-07-21 17:12:06 +0300

    fix

--- src/components/shoes.jsx
+++ src/components/shoes.jsx
@@ -8,7 +8,7 @@ export default function Shoes() {
           <img src="pic/purpule.png" alt="purpule" />
           <p className="degem">דגם 13</p>
           <p><b> נותר במידות: </b></p>
-          <p>c4 , c8 , c11  ,  c12 </p>
+          <p>c4 , c8 ,c10 , c11  ,  c12 , c13 </p>
         </div>
 
         <div className="card">
@@ -130,7 +130,7 @@ export default function Shoes() {
           <img src="pic/brownk.png" alt="pgen" />
           <p className="degem">דגם 34</p>
           <p><b> נותר במידות: </b></p>
-          <p>  c12, c13  </p>
+          <p>  c13  </p>
         </div>
 
       </div>
