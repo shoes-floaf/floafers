082725ad50b066fb9aa558b1cea4301e45ebea15
chanaGreenfeld <c64966@gmail.com>
2025-07-22 20:56:31 +0300

    remove c8

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
             </th>
             <th className='azal1'>c6 <div className="ellipse-outline">אזל!</div></th>
             <th className='azal1'>c7 <div className="ellipse-outline">אזל!</div></th>
-            <th>c8</th>
+            <th className='azal1'>c8 <div className="ellipse-outline">אזל!</div></th>
             <th>c9</th>
             <th>c10</th>
             <th>c11</th>
