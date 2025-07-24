acc827d1c9a0d5f78b8ebe3b150bacd99cf5dbbe
chanaGreenfeld <c64966@gmail.com>
2025-07-24 22:45:49 +0300

    fix

--- src/components/shoes.jsx
+++ src/components/shoes.jsx
@@ -7,132 +7,161 @@ export default function Shoes() {
         <div className="card">
           <img src="pic/purpule.png" alt="purpule" />
           <p className="degem">דגם 13</p>
-          <p><b> נותר במידות: </b></p>
-          <p> c10 , c11  ,  c12 , c13 </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p>c4, c10 , c11 , c12 , c13 </p>
         </div>
 
         <div className="card">
-        <img src="pic/pink.png" alt="pink" />
-        <p className="degem">דגם 14</p>
-        <p><b> נותר במידות: </b></p>
-        <p>c4 , c9  ,  c10  , c11  ,  c12  ,  j1</p>
+          <img src="pic/pink.png" alt="pink" />
+          <p className="degem">דגם 14</p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p>c4 , c9 , c10 , c11 , c12 , j1</p>
         </div>
 
         <div className="card">
           <img src="pic/orange.png" alt="orange" />
           <p className="degem">דגם 15</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c11  , c13  ,  j1</p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c11 , c13 , j1</p>
         </div>
-
 
         <div className="card">
           <img src="pic/points.png" alt="points" />
           <p className="degem">דגם 16</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c10  , c11  ,  c12  ,  c13  </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c10 , c11 , c12 , c13 </p>
         </div>
 
-        
         <div className="card">
           <img src="pic/whiteGray.png" alt="whiteGray" />
           <p className="degem">דגם 17</p>
-          <p><b> נותר במידות: </b></p>
-          <p>   c11  </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c11 </p>
         </div>
-
 
         <div className="card">
           <img src="pic/brown.png" alt="brown" />
           <p className="degem">דגם 18</p>
-          <p><b> נותר במידות: </b></p>
-          <p> c4 ,  c11 </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c4 , c11 </p>
         </div>
-
 
         <div className="card">
           <img src="pic/dolphin.png" alt="dolphin" />
           <p className="degem">דגם 19</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c10  ,  c11  ,  c12  ,  c13 </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c10 , c11 , c12 , c13 </p>
         </div>
-
 
         <div className="card">
           <img src="pic/grayYellow.png" alt="grayYellow" />
           <p className="degem">דגם 20</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c11 , c13  </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c11 , c13 </p>
         </div>
 
         <div className="card">
           <img src="pic/greenBlue.png" alt="greenBlue" />
           <p className="degem">דגם 22</p>
-          <p><b> נותר במידות: </b></p>
-          <p> c4  </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c4 </p>
         </div>
 
         <div className="card">
           <img src="pic/whiteBlue.png" alt="whiteBlue" />
           <p className="degem">דגם 23</p>
-          <p><b> נותר במידות: </b></p>
-          <p> c4 , c9 ,  c10  ,  c12 </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c4 , c9 , c10 , c12 </p>
         </div>
 
         <div className="card">
           <img src="pic/whitePink.png" alt="whitePink" />
           <p className="degem">דגם 24</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c11  , c12 , c13 </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c11 , c12 , c13 </p>
         </div>
 
         <div className="card">
           <img src="pic/gray.png" alt="gray" />
           <p className="degem">דגם 26</p>
-          <p><b> נותר במידות: </b></p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
           <p> c4 , c11 </p>
         </div>
 
         <div className="card">
           <img src="pic/chayal.png" alt="chayal" />
           <p className="degem">דגם 27</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c11 </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c11 </p>
         </div>
         <div className="card">
           <img src="pic/lightgray.png" alt="lightgray" />
           <p className="degem">דגם 28</p>
-          <p><b> נותר במידות: </b></p>
-          <p>   c11  ,  c12  ,  c13 </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c11 , c12 , c13 </p>
         </div>
-
 
         <div className="card">
           <img src="pic/camelK.png" alt="camelK" />
           <p className="degem">דגם 29</p>
-          <p><b> נותר במידות: </b></p>
-          <p>   c12 , c13</p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c12 , c13</p>
         </div>
 
         <div className="card">
           <img src="pic/purpuleKi.png" alt="purpuleK" />
           <p className="degem">דגם 30</p>
-          <p><b> נותר במידות: </b></p>
-          <p>   c12  ,  c13  </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c12 , c13 </p>
         </div>
         <div className="card">
           <img src="pic/green.png" alt="pgen" />
           <p className="degem">דגם 33</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c10  </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c10 </p>
         </div>
         <div className="card">
           <img src="pic/brownk.png" alt="pgen" />
           <p className="degem">דגם 34</p>
-          <p><b> נותר במידות: </b></p>
-          <p>  c13  </p>
+          <p>
+            <b> נותר במידות: </b>
+          </p>
+          <p> c13 </p>
         </div>
-
       </div>
     </>
   )
