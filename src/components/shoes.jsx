165bf201b33faacc1f0f993a7e940e7adf6e21d7
chanaGreenfeld <c64966@gmail.com>
2025-07-27 17:56:08 +0300

    fix

--- src/components/shoes.jsx
+++ src/components/shoes.jsx
@@ -126,7 +126,7 @@ export default function Shoes() {
           <p>
             <b> נותר במידות: </b>
           </p>
-          <p> c11 , c12 , c13 </p>
+          <p> c11 , c12  </p>
         </div>
 
         <div className="card">
